f1ffc928a42f70b1a7f65612c05fee9b1da73699
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-08-21 09:41:53 +0800

    Added echo feature and chat exits only if "bye" is input

--- src/main/java/Geegar.java
+++ src/main/java/Geegar.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Geegar {
     public static void main(String[] args) {
         int UNDERSCORE_LENGTH = 60;
@@ -7,8 +9,24 @@ public class Geegar {
         System.out.println("What can I do for you today?");
         System.out.println("_".repeat(UNDERSCORE_LENGTH));
 
+        Scanner sc = new Scanner(System.in);
+
+        // While loop to continue waiting for user input until user ends chat with "bye"
+        boolean isActive = true;
+        while (isActive) {
+            String input = sc.nextLine();
+
+            System.out.println("_".repeat(UNDERSCORE_LENGTH));
+            System.out.println(" " + input);
+            System.out.println("_".repeat(UNDERSCORE_LENGTH));
+
+            if (input.toLowerCase().equals("bye")) {
+                isActive = false;
+            }
+        }
+
         // saying goodbye
-        String goodbye = "Alright Bye! Have a Geeky Time!\n" + "_".repeat(UNDERSCORE_LENGTH);
+        String goodbye = "Alright Bye !! :3 Have a Geeky Time!\n" + "_".repeat(UNDERSCORE_LENGTH);
         System.out.println(goodbye);
 
     }
