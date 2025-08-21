05544331b76c6a69a87b49e69436e6bc274d366d
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-08-21 10:13:05 +0800

    feat: Added ability to store a list and repeat the list

--- src/main/java/Geegar.java
+++ src/main/java/Geegar.java
@@ -3,30 +3,44 @@ import java.util.Scanner;
 public class Geegar {
     public static void main(String[] args) {
         int UNDERSCORE_LENGTH = 60;
+
+        String[] listStorage = new String[100];
+        int index = 0;
         // saying introduction
         System.out.println("_".repeat(UNDERSCORE_LENGTH));
-        System.out.println("Hello! I'm Geegar");
+        System.out.println("Hello! I'm Geegar \uD83E\uDDCC");
         System.out.println("What can I do for you today?");
         System.out.println("_".repeat(UNDERSCORE_LENGTH));
 
         Scanner sc = new Scanner(System.in);
 
         // While loop to continue waiting for user input until user ends chat with "bye"
-        boolean isActive = true;
-        while (isActive) {
+        while (true) {
             String input = sc.nextLine();
 
-            System.out.println("_".repeat(UNDERSCORE_LENGTH));
-            System.out.println(" " + input);
-            System.out.println("_".repeat(UNDERSCORE_LENGTH));
-
             if (input.toLowerCase().equals("bye")) {
-                isActive = false;
+                break;
             }
+
+            if (input.toLowerCase().equals("list")) {
+                System.out.println("_".repeat(UNDERSCORE_LENGTH));
+                System.out.println("\uD83E\uDDCC");
+                for (int i = 0; i < index; i++) {
+                    System.out.println(i + 1 + "." + listStorage[i]);
+                }
+                System.out.println("_".repeat(UNDERSCORE_LENGTH));
+            }
+
+            listStorage[index] = input;
+            index++;
+
+            System.out.println("_".repeat(UNDERSCORE_LENGTH));
+            System.out.println("\uD83E\uDDCC: added: " + input);
+            System.out.println("_".repeat(UNDERSCORE_LENGTH));
         }
 
         // saying goodbye
-        String goodbye = "Alright Bye !! :3 Have a Geeky Time!\n" + "_".repeat(UNDERSCORE_LENGTH);
+        String goodbye = "_".repeat(UNDERSCORE_LENGTH) + "\n \uD83E\uDDCC: Alright Bye !! :3 Have a Geeky Time!\n" + "_".repeat(UNDERSCORE_LENGTH);
         System.out.println(goodbye);
 
     }
