a1e463d49ea5bcb6156f43ae3f79c7654ce21c11
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-08-26 22:35:19 +0800

    feat: add a new constructor to accept boolean for each respective boolean types

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,12 @@ public class Event extends Task {
         this.to = to;
     }
 
+    public Event(String description, String from, String to, Boolean isDone) {
+        super(description, isDone);
+        this.from = from;
+        this.to = to;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
