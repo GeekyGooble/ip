10a839089a218d41cf341eeb5b2634ed3a6b5854
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-08-27 21:46:10 +0800

    feat: add TaskList.java

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,67 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task delete(int index) throws InvalidTaskNumberException {
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new InvalidTaskNumberException(String.valueOf(index + 1));
+        }
+        return this.tasks.remove(index);
+    }
+
+    public Task get(int index) {
+        return this.tasks.get(index);
+    }
+
+    public void markTask(int index) throws InvalidTaskNumberException {
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new InvalidTaskNumberException(String.valueOf(index + 1));
+        }
+        tasks.get(index).markAsDone();
+    }
+
+    public void unmarkTask(int index) throws InvalidTaskNumberException {
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new InvalidTaskNumberException(String.valueOf(index + 1));
+        }
+        tasks.get(index).markNotDone();
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return this.tasks.isEmpty();
+    }
+
+    public void showTasksOnDate(LocalDate date) {
+        for (Task task : this.tasks) {
+            if (task instanceof Deadline) {
+                Deadline d = (Deadline) task;
+                if (d.by.toLocalDate().equals(date)) {
+                    System.out.println(d);
+                }
+            } else if (task instanceof Event) {
+                Event e = (Event) task;
+                if (e.from.toLocalDate().equals(date) || e.to.toLocalDate().equals(date)) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
