a1a8a67b7c9187822eb7beb923bbea83d31dc200
GeekyGooble <tjh.tanjunyao@gmail.com>
2025-09-09 21:34:33 +0800

    add Assertions under Command Classes
    Currently, the Command classes assume that their inptus are always valid and other utility classes for execution, (TaskList, Gui, Storage) never null. However, this is only implicit in the code.
    
    Without explicit documentation or runtime checks, programmer errors may occur when failing to do proper user checks under the Parser Class, this errors may go unoticed and make it harder to debug in the future.
    
    Add Java `assert` statements in consturctors and execute() methods to check invariants (like non-null parameters, positive task numbers). This enforces these assumptions during development using the -ea command.
    
    Use `assert` instead of if-else beacuse these checks guards against programmer mistakes, not user input errors (which should have been captured via the Parser class before passing into the Command Class.)
    
    This also potentially helps to improve code readability by documenting necessary assumptions direclty in the code to catch bugs earleir. Assertions can also be easily disabled in production without affecting runtime performance.

--- src/main/java/geegar/command/DeleteCommand.java
+++ src/main/java/geegar/command/DeleteCommand.java
@@ -13,13 +13,22 @@ public class DeleteCommand extends Command {
 
     private int taskNumber;
 
+    /**
+     * Creates an instance of a DeleteCommand using the input taskNumber
+     * @param taskNumber
+     */
     public DeleteCommand(int taskNumber) {
-
+        assert taskNumber > 0 : "Task number must be positive";
         this.taskNumber = taskNumber;
     }
 
     @Override
     public void execute(TaskList tasks, Gui gui, Storage storage) throws GeegarException {
+
+        assert tasks != null : "TaskList in DeleteCommand must not be null";
+        assert gui != null : "Gui in DeleteCommand must not be null";
+        assert storage != null : "Storage in DeleteCommand must not be null";
+
         Task deletedTask = tasks.delete(taskNumber - 1);
         storage.save(tasks.getTasks());
         gui.printTaskDeleted(deletedTask, tasks.size());
